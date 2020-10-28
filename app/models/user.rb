136a5383288906f675a64f4fb54c1ad0346ea591
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-10-28 15:52:37 +0900

    passwordに対して半角英数を組み合わせのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
   validates :nickname, presence: true, length: { maximum: 40 }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+    validates :first_name
+    validates :last_name
   end
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-    validates :first_kana, presence: true
-    validates :last_kana, presence: true
+    validates :first_kana
+    validates :last_kana
   end
   validates :birthday, presence:true
 
