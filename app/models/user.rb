a3de35a180adc18ba8943456fc7534f703f64a24
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-10-28 15:37:37 +0900

    factorybotに代入及びuserモデルバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 40 }
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_kana, presence: true
-  validates :last_kana, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :first_kana, presence: true
+    validates :last_kana, presence: true
+  end
   validates :birthday, presence:true
 
   has_many :itmes
