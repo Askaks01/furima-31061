3470c13dc491ab5ba3e8e56d4f084c3889ee9380
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-10-29 18:38:34 +0900

    validationの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+  
   def index
+    @itmes = Item.order("created_at DESC")
   end
+
+  def new
+    @itme = Item.new
+  end
+
+  def create
+    @item = item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private 
+  
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :prefecture_id, :category_id, :condition_id, :postage_id, :shipping_day_id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture,
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :shipping_day
+  belongs_to :user
+  has_one_attached :image
+  has_one :purchase_history
 
-  validates :prefecture,     presence: true
+  validates :name, :price, :description, :prefecture, :category, :condition, :postage, :shipping_day,  presence: true
+  validates :prefecture_id, :category_id, :condition_id, :postage_id, :shipping_day_id, numericality: { other_than: 1 }
 end
