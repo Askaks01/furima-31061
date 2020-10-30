85bdf818dda459aadea5d5303f68a72d3f72c70f
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-10-30 18:04:31 +0900

    itemモデルの単体テスト完了

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,86 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it 'name,image,category,price,condition,description,postage,prefecture,shipping_dayが存在していれば保存できること' do
+      expect(@item).to be_valid
+    end
+
+    it 'nameが空では保存できないこと' do
+    @item.name = nil
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'imageが空では保存できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it 'priceが空では保存できないこと' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'priceが半角数字でないと保存できないこと' do
+      @item.price = "３００"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+
+    it 'priceが300以上でないと保存できないこと' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+
+    it 'priceが9999999以下でないと保存できないこと' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+
+    it 'descriptionが空では保存できないこと' do
+      @item.description = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+    it 'categoryが---では保存できないこと' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+    
+    it 'prefectureが---では保存できないこと' do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+    end
+
+    it 'condition---では保存できないこと' do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition must be other than 1")
+    end
+
+    it 'postageが---では保存できないこと' do
+      @item.postage_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Postage must be other than 1")
+    end
+
+    it 'Shippingが_day---では保存できないこと' do
+      @item.shipping_day_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
+    end
+  end
 end
+
