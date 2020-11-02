feb8d0c7457149959d3caec99970711811ad7c0b
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-02 12:47:23 +0900

    コンフリクト解除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
   def index
-<<<<<<< Updated upstream
-=======
     @items = Item.order("created_at DESC")
->>>>>>> Stashed changes
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else 
+      render :new
+    end
+  end
+
+  private 
+  
+  def item_params
+    params.require(:item).permit(:name, :price, :image, :description, :prefecture_id, :category_id, :condition_id, :postage_id, :shipping_day_id).merge(user_id: current_user.id)
+  end
+
 end
