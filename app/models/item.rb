3019941ea9100aef7149ef1eeed51572b2af06d1
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-02 11:47:06 +0900

    ハッシュシンボルに変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,8 +13,8 @@ class Item < ApplicationRecord
     validates :name, length: { maximum: 40 }
     validates :price, 
     :numericality => {
-      :greater_than_or_equal_to => 300,
-      :less_than_or_equal_to => 9999999
+      greater_than_or_equal_to: 300,
+      less_than_or_equal_to: 9999999
     }, 
     format: { with: /\A[0-9]+\z/ }
     validates :image
