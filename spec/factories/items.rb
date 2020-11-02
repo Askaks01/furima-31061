50ba5a52c8e177ab7d045b4f22bb7da2f3802f8e
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-02 12:23:40 +0900

    indexのインスタンス変数削除とfactory(items)データの一部修正実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     postage_id {2}
     prefecture_id {2}
     shipping_day_id {2}
-    price {"1000000"}
+    price {1000000}
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
