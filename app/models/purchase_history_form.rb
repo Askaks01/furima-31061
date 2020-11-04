163edc9153a6019fffea73a628a585116710dc77
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-04 14:33:35 +0900

    マイグレーションファイルの作成

---
+++ app/models/purchase_history_form.rb
@@ -0,0 +1,6 @@
+class ItemPurchaseHistory
+
+  include ActiveModel::Model
+  attr_accessor :item, :user, :postal_code, :prefecture_id, :city, :house_num, :building_name, :phone_num, :purchase_history
+
+end
