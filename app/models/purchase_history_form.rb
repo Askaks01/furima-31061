7d98cf72c9496fe1708bb8e25c81942163ff8a32
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-04 15:56:43 +0900

    バリデーションの移動

--- app/models/purchase_history_form.rb
+++ app/models/purchase_history_form.rb
@@ -3,9 +3,19 @@ class ItemPurchaseHistory
   include ActiveModel::Model
   attr_accessor :item, :user, :postal_code, :prefecture_id, :city, :house_num, :building_name, :phone_num, :purchase_history
 
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}\-?d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city, 
+    validates :house_num,
+    validates :phone_num, format: { with: /\A\d{10,11}\z/ }
+  end
+  validates :building_name
+
 
   def save
-
+    purchase_history = PurchaseHistory.create(user_id: current_user.id, item_id: item.id) 
+    purchase_info = PurchaseInfo.create
   end
 
 end
--- app/models/purchase_info.rb
+++ app/models/purchase_info.rb
@@ -1,15 +1,4 @@
 class PurchaseInfo < ApplicationRecord
   belongs_to :purchase_history
 
-  with_options presence: true do
-    validates :postal_code,
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :city, 
-    validates :house_num,
-    validates :phone_num,
-  end
-
-  validates :building_name,
-
-
 end
