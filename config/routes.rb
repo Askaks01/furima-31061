7060f1be323111b1882fc2072fa3c68293d1b70d
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-04 16:04:17 +0900

    ネストされたルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'purchase_histories/index'
   devise_for :users
   root to: "items#index"
-  resources :items
+  resources :items do
+    resources :purchase_histories
+  end
 end
