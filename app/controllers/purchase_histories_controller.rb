39b020dd28d9a64e762f6f0cac3089a6151c9f2b
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-04 19:40:06 +0900

    formwithの編集

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,10 +1,10 @@
 class PurchaseHistoriesController < ApplicationController
   def index
-    @item_purchase_history = ItemPurchaseHistory.new
+    @item = Item.find(params[:id])
+    @purchase_history_form = PurchaseHistoryForm.new
   end
 
   def create
-    @purchase_history = PurchaseHistory.create(purchase_history_params)
   end
 
 end
--- app/models/purchase_history_form.rb
+++ app/models/purchase_history_form.rb
@@ -1,4 +1,4 @@
-class ItemPurchaseHistory
+class PurchaseHistoryForm
 
   include ActiveModel::Model
   attr_accessor :item, :user, :postal_code, :prefecture_id, :city, :house_num, :building_name, :phone_num, :purchase_history
@@ -6,12 +6,10 @@ class ItemPurchaseHistory
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}\-?d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
-    validates :city, 
-    validates :house_num,
+    validates :city 
+    validates :house_num
     validates :phone_num, format: { with: /\A\d{10,11}\z/ }
   end
-  validates :building_name
-
 
   def save
     purchase_history = PurchaseHistory.create(user_id: current_user.id, item_id: params[:item_id]) 
