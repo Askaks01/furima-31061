4c4a03012a398bd732307e709ef165f15a746ddc
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-05 11:18:18 +0900

    purchase_historyコントローラ#createアクションの作成

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -5,6 +5,12 @@ class PurchaseHistoriesController < ApplicationController
   end
 
   def create
+    @purchase_history_form = PurchaseHistoryForm.new(purchase_history_form_params)
+    if @purchase_history_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
 end
