8202f5b6573565f200528b7e7e97c1230092da4e
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-09 12:36:16 +0900

    記述誤りを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: :edit
   before_action :sold_out, only: :edit
-  before_action :item_find only: [:edit, :update, :show]
+  before_action :item_find, only: [:edit, :update, :show]
 
   def index
     @items = Item.order('created_at DESC')
