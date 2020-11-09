1e22ddd4322dcd13af1992eb60280400f6431a38
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-11-09 14:24:35 +0900

    move_to_indexのdestory適用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, only: [:edit, :destory]
   before_action :sold_out, only: :edit
   before_action :item_find, only: [:edit, :update, :show, :destroy, :move_to_index, :sold_out]
 
